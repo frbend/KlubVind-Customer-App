e35408409859be81087e2b137f984c51d13fe529
frbend <62542322+frbend@users.noreply.github.com>
2022-07-25 12:52:37 +0200

    deploy config

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -9,16 +9,17 @@ class Search extends React.Component{
         super(props);
         this.state = {
             responseData: [],
-            search: ""
+            search: "",
+            value: ''
         };
     }
     //store ID in a state
 
 
-
+    //for search
 
     getData = () => {
-        Axios.get(`http://localhost:8080/list/Kommune/` + this.state.search)
+        Axios.get(`https://klubvind-call-list.herokuapp.com/list/Kommune/` + this.state.search)
           .then(responseData => {
               if(responseData.data.length === 0){
                 console.warn("No results")
@@ -34,10 +35,10 @@ class Search extends React.Component{
         //console.log(event.target.value);
     }
 
-    //save responseData[].id in a state
+    //for switching between previous and next line
 
     prevItem =() =>{
-        Axios.get(`http://localhost:8080/list/Kommune/:Kommune/prev/`+ this.state.responseData[0].id)
+        Axios.get(`https://klubvind-call-list.herokuapp.com/list/Kommune/:Kommune/prev/`+ this.state.responseData[0].id)
         .then(prevData =>{
             this.setState({responseData: prevData.data});
             console.log(prevData.data[0])
@@ -45,11 +46,38 @@ class Search extends React.Component{
 }
 
     nextItem =() =>{
-        Axios.get(`http://localhost:8080/list/Kommune/:Kommune/next/`+ this.state.responseData[0].id)
+        Axios.get(`https://klubvind-call-list.herokuapp.com/list/Kommune/:Kommune/next/`+ this.state.responseData[0].id)
         .then(newData =>{
             this.setState({responseData: newData.data});
             console.log(newData.data[0])
         })
+    }
+
+    //for adding note
+
+    handleChange = event => {
+        event.preventDefault();
+        this.setState({value: event.target.value})
+    }
+
+    ///list/Kommune/:Kommune/:id/Note/:Note
+
+    getNote = () => {
+        if(this.state.responseData[0].Note !== null){
+            console.log("Note is: " + this.state.value + ' , ' + this.state.responseData[0].Note);
+            Axios.post('http://localhost:8080/list/Kommune/'+ this.state.responseData[0].Kommune + '/' + this.state.responseData[0].id + '/Note/' + this.state.value)
+                .then(updatedNote =>{
+                    this.setState({Note: updatedNote + this.state.responseData[0].Note});
+                    console.log(updatedNote);
+                })
+        }else{
+            console.log("Note is empty, adding current input: " + this.state.value)
+                Axios.post('http://localhost:8080/list/Kommune/'+ this.state.responseData[0].Kommune + '/' + this.state.responseData[0].id + '/Note/' + this.state.value)
+                    .then(newNote =>{
+                        this.setState({Note : newNote})
+                        console.log(newNote.data.message + " Note added : " + this.state.value)
+                    })
+        }
     }
 
 
@@ -60,8 +88,8 @@ class Search extends React.Component{
             <input type="text" onChange={this.onChange} value={this.state.search}/>
             <button onClick={this.getData} className="btn btn-primary">Search</button><br />
             <div className="button-box col-lg-12">
-                <button onClick={this.nextItem} className="btn btn-primary ">Next</button>
                 <button onClick={this.prevItem} className="btn btn-primary ">Previous</button>
+                <button onClick={this.nextItem} className="btn btn-primary ">Next</button>
             </div>
             <table className="table">
                 <thead>
@@ -72,7 +100,7 @@ class Search extends React.Component{
                     <th scope="col">Kontakt skrab</th>
                     <th scope="col">Kontakt</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Telefon</th>
+                    <th scope="col">Telefon</th>                    
                     </tr>
                 </thead>
                 <tbody>
@@ -89,6 +117,27 @@ class Search extends React.Component{
                     ))}
                 </tbody>
             </table>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Note</th>
+                    </tr>
+                </thead>
+            <tbody>
+            {this.state.responseData.map(responseData =>(
+                        <tr key={responseData.id}>
+                            <td>{responseData.Note}</td>
+                        </tr>
+                    ))}
+            </tbody>
+            </table>
+            <div>
+            <label>
+              Add a note:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <button onClick={this.getNote}>Post Note</button>
+          </div>
         </div>
         );
     }
