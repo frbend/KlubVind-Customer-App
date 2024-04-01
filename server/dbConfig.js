ae54f2e65d904b20fb28b5c9ba75ce85ce2001d2
frbend <62542322+frbend@users.noreply.github.com>
2024-04-01 14:25:58 +0200

    deploy config

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 
 //not needed for production -- the build still doesn't work -- need to do something about @azure/msal-node
-//const connectData = require ('./config.json')
+const connectData = require ('./config.json')
 
-const connection = mysql.createConnection( {
+require('dotenv').config();
+
+const connection = mysql.createConnection({
     host: process.env.host || connectData.host,
     user: process.env.user || connectData.user,
     password: process.env.password || connectData.password,
     database: process.env.database || connectData.database,
     port: process.env.port || connectData.port
-})
+});
 
 
 connection.connect(function(err){
